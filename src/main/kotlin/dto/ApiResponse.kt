f61a91fb819a30ed147efc78c02286875f772be7
lesam19092 <danigpro1337@gmail.com>
2024-09-16 11:21:59 +0300

    Добавил data класс для получения информации с запроса

---
+++ src/main/kotlin/dto/ApiResponse.kt
@@ -0,0 +1,10 @@
+package dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ApiResponse(
+    @SerialName("results")
+    val results: List<News>? = null
+)
